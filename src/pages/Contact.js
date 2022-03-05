9f9b976771d1e142da470b52ad11b27d17d6a280
mbluis4 <92177912+mbluis4@users.noreply.github.com>
2022-03-05 16:00:12 -0300

    Talwinds css
    Added tailwinds

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,20 +8,53 @@ const Contact = () => {
       message: "",
     },
   ]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+    //console.log(name, value);
+  };
+
   return (
-    <>
-      <form action>
+    <div className="bg-white space-y-10 py-6 shadow-lg rounded-lg">
+      <form action="#" className="py-2 px-6">
+        <label for="name">Nombre</label>
         <input
-          placeholder="Nombre"
+          type="text"
           name="name"
           value={form.name}
-          /* onChange={handleChange} */
+          onChange={handleChange}
+          className="mt-1 mb-2 border-2 py-1 rounded-lg shadow-sm w-full"
         ></input>
-        <input type="email" placeholder="tu email"></input>
-        <textarea type="text" placeholder="tu mensaje"></textarea>
-        <button type="Submit">Enviar</button>
+        <label for="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          className="my-1 border-2 py-1 rounded-lg shadow-sm w-full"
+          required
+        ></input>
+        <label for="message">Mensaje</label>
+        <textarea
+          type="text"
+          placeholder="mensaje"
+          name="message"
+          value={form.message}
+          onChange={handleChange}
+          rows="8"
+          className="placeholder:italic placeholder:text-slate-400 w-full border resize-none"
+        ></textarea>
+        <button
+          type="Submit"
+          className="block border-2 shadow-sm mx-auto px-6 py-2 rounded-lg mt-2"
+        >
+          Enviar
+        </button>
       </form>
-    </>
+    </div>
   );
 };
 
